2897a8067033151bacf13549c5f52cd0d7782476
Asuka <5rho@users.noreply.github.com>
2025-08-05 18:47:38 +0900

    20508051847

--- modify.py
+++ modify.py
@@ -1,17 +1,3 @@
-"""
-#シート名確認
-import sqlite3
-
-conn = sqlite3.connect("sensor_data.db")
-cursor = conn.cursor()
-
-# DBに含まれるすべてのテーブル名を表示
-cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
-tables = cursor.fetchall()
-
-print("テーブル一覧:", tables)
-conn.close()
-"""
 import os
 db_path = os.path.abspath("sensor_data.db")
 print("接続中のDBパス:", db_path)
@@ -24,15 +10,11 @@ print("DB内テーブル一覧:", cursor.fetchall())
 
 
 #データベース確認
-import pandas as pd
 import sqlite3
-
+import pandas as pd
 conn = sqlite3.connect("sensor_data.db")  # ← 実際のDBファイル名に置き換えてください
 df = pd.read_sql_query("SELECT * FROM measurements", conn)
-
 print(df.head())  # 中身を確認
-
-
 
 
 
@@ -45,15 +27,6 @@ df = pd.read_sql_query("SELECT * FROM sensor_measurements", conn)
 
 
 
-"""
-df['new_column'] = df.apply(
-    lambda row: row['colA'] * row['colB'] if pd.isna(row['new_column']) else row['new_column'], 
-    axis=1
-)
-
-    di = 0.81 * temperature + 0.01 * humidity * (0.99 * temperature - 14.3) + 46.3
-    return round(di, 2)
-"""
 
 
 
